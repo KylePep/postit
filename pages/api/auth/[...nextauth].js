090c94ff32b2378a1f882943e7881a293cb39b0b
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-01 10:39:37 -0700

    setting up google provider connection

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+// import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '../../../prisma/client.js'
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+})
+
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+// }
+
+// export default NextAuth(authOptions)
