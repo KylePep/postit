dd2a9e9bd237ba83712ccf997b9350bd966ce71f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-02 02:56:17 -0700

    retrieving google user info, displaying avatar image

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -3,17 +3,18 @@ import Login from "./Login";
 import {getSession} from 'next-auth/react'
 import {authOptions} from '../../pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth";
+import Logged from "./Logged";
 
 export default async function Nav(){
   const session = await getServerSession(authOptions)
-  console.log('[NAV SESSION]', session)
   return (
   <nav className="flex justify-between items-center py-8">
     <Link href={"/"}>
       <h1 className="font-bold text-lg">Send it.</h1>
     </Link>
     <ul className="flex item-center gap-6">
-      <Login/>
+      {!session?.user && <Login/>}
+      {session?.user && <Logged image={session.user?.image || ""}/>}
     </ul>
   </nav>
   )
