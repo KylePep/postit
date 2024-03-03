da5d8e3ea5236afc256c1ad3e7cdaf015a622bac
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-03 02:12:24 -0700

    created submit button and styling to AddPost

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -4,10 +4,14 @@ import { useState } from "react"
 
 export default function CreatePost(){
   const [title, setTitle] = useState("")
+  const [isDisabled, setIsDisabled] = useState(false)
   return (
-    <form action="">
+    <form className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea onChange={(e) => setTitle(e.target.value)} name="title" value={title} placeholder="What's on your mind?" className="p-4 text-lg rounded-md my-2 bg-gray-200"></textarea>
+      </div>
       <div>
-        <textarea onChange={(e) => setTitle(e.target.value)} name="title" value={title} placeholder="What's on your mind?"></textarea>
+        <button disabled={isDisabled} className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25" type="submit">Create a post</button>
       </div>
     </form>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import AddPost from "./components/AddPost"
 export default function Home() {
   return (
     <main className="">
-      <AddPost/>
       <h1>Hello Next</h1>
+      <AddPost/>
     </main>
   );
 }
