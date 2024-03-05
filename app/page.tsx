0e9daa3c8dbe69a458a456481b27aaf665db2ca9
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-05 06:41:37 -0700

    Getting post from api, created post component and comment model

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 'use client'
+import axios from "axios";
 import AddPost from "./components/AddPost"
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Posts";
+
+//Fetch all posts
+const allPosts = async () =>{
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
 
 export default function Home() {
+  const {data, error, isLoading} = useQuery({queryFn: allPosts, queryKey: ["posts"]})
+  if (error) return error
+  if(isLoading) return "Loading..."
+  console.log(data)
   return (
     <main className="">
-      <h1>Hello Next</h1>
       <AddPost/>
+      {data?.map((post)=> (
+        <Post key={post.id} name={post.user.name} avatar={post.user.image} postTitle={post.title} id={post.id}/>
+      ))}
     </main>
   );
 }
