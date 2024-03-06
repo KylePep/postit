fe97cc8d617e0a2f91341b0aa50f0a93d923ccc5
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-06 05:23:38 -0700

    Changing PrismaAdapter to next-auth/prisma-adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-// import GithubProvider from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from '../../../prisma/client'
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+const prisma = new PrismaClient();
 
 const adapter = PrismaAdapter(prisma)
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -16,18 +17,4 @@ export const authOptions = {
   ],
 }
 
-// secret: process.env.AUTH_SECRET,
 export default NextAuth(authOptions)
-
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-// }
-
-// export default NextAuth(authOptions)
